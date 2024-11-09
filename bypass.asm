cb9187643f7d673dd6009782bf9391729a090842
TakuikaNinja <takuikaninja@gmail.com>
2024-11-09 16:05:59 +1300

    Initial working build

---
+++ bypass.asm
@@ -0,0 +1,71 @@
+PPUCTRL = $2000
+NMI_1 = $dff6
+NMI_2 = $dff8
+NMI_3 = $dffa
+RST_FLAG = $0102
+RST_TYPE = $0103
+FDS_RESET = $fffc
+Ptr = $76 ; $76~$77 are always cleared on reset, so clobbering it won't matter
+FDS_CTRL_MIRROR = $fa
+FDS_CTRL = $4025
+
+; "NMI" routine which is entered to bypass the BIOS check
+Bypass:
+		lda #$00										; disable NMIs since we don't need them anymore
+		sta PPUCTRL
+		
+		lda Vectors										; put real NMI handler in NMI vector 3
+		sta NMI_3
+		lda Vectors+1
+		sta NMI_3+1
+		
+		lda #$35										; tell the FDS that the BIOS "did its job"
+		sta RST_FLAG
+		lda #$ac
+		sta RST_TYPE
+		
+		jsr FixPointers
+		jmp (FDS_RESET)									; jump to reset FDS
+
+; This game uses data in the $0100~$012f region of memory, 
+; which conflicts with FDS BIOS variables at $0100~$0103.
+; Add $10 to the low byte of each pointer value so they use $0110~$013f instead.
+FixPointers:
+		lda #$00
+		tay
+		ldx #(Pointers_Hi - Pointers_Lo - 1)
+-
+		lda Pointers_Lo,x
+		sta Ptr
+		lda Pointers_Hi,x
+		sta Ptr+1
+		lda (Ptr),y
+		clc
+		adc #$10
+		sta (Ptr),y
+		dex
+		bpl -
+		rts
+
+Pointers_Lo:
+	.db <$824e, <$827f, <$8565, <$8859, <$885f, <$a14e, <$a214, <$a228, <$a22e, <$a233, <$a5cc
+
+Pointers_Hi:
+	.db >$824e, >$827f, >$8565, >$8859, >$885f, >$a14e, >$a214, >$a228, >$a22e, >$a233, >$a5cc
+
+; New reset handler which sets vertical mirroring (horrizontal arrangement)
+Reset:
+		lda FDS_CTRL_MIRROR								; get setting previously used by FDS BIOS
+		and #$f7										; and set for vertical mirroring
+		sta FDS_CTRL
+	.db $4c ; JMP Absolute
+	.incbin "Ikki (Japan).nes", INES_HDR + prg_length - 4, 2 ; jump to original reset handler
+
+.org NMI_1
+Vectors:
+	.incbin "Ikki (Japan).nes", INES_HDR + prg_length - 6, 2 ; NMI #1
+	.incbin "Ikki (Japan).nes", INES_HDR + prg_length - 6, 2 ; NMI #2
+	.dw Bypass ; NMI #3, entry point
+	.dw Reset ; reset
+	.incbin "Ikki (Japan).nes", INES_HDR + prg_length - 2, 2 ; IRQ (unused?)
+
---
+++ ikki.asm
@@ -0,0 +1,103 @@
+; ==================================================================================================================================
+; ----------------------------------------------------------------------------------------------------------------------------------
+; Ikki FDS Port Disk Layout
+; ----------------------------------------------------------------------------------------------------------------------------------
+; Disk Structure
+	.org $0000
+	INES_HDR = $10 ; size of iNES header
+	
+; ----------------------------------------------------------------------------------------------------------------------------------
+; Disk definitions taken from SMB2J's disassembly
+	DiskInfoBlock     = 1
+	FileAmountBlock   = 2
+	FileHeaderBlock   = 3
+	FileDataBlock     = 4
+	PRG = 0
+	CHR = 1
+	VRAM = 2
+	FILE_COUNT = 5
+
+; ----------------------------------------------------------------------------------------------------------------------------------
+; Disk info + file amount blocks
+	.db DiskInfoBlock
+	.db "*NINTENDO-HVC*"
+	.db 0												; manufacturer
+	.db "RIK "											; game title + space for normal disk
+	.db 0, 0, 0, 0, 0									; game version, side, disk, disk type, unknown
+	.db FILE_COUNT										; boot file count
+	.db $ff, $ff, $ff, $ff, $ff
+	.db $60, $11, $28									; cart release date according to nescartdb (1985/11/28)
+	.db $49, $61, 0, 0, 2, 0, 0, 0, 0, 0				; region stuff
+	.db $99, $11, $09									; use disk write date as date of port release for now
+	.db 0, $80, 0, 0, 7, 0, 0, 0, 0						; unknown data, disk writer serial no., actual disk side, price
+
+	.db FileAmountBlock
+	.db FILE_COUNT
+
+; ----------------------------------------------------------------------------------------------------------------------------------
+; PRG
+	.db FileHeaderBlock
+	.db $00, $00
+	.db "IKKIPRGM"
+	.dw $8000
+	.dw prg_length
+	.db PRG
+	
+	.db FileDataBlock
+	prg_length = $4000 ; 16KiB
+	.incbin "Ikki (Japan).nes", INES_HDR, prg_length
+	
+; Entry Point + Interrupt Vectors
+	.db FileHeaderBlock
+	.db $01, $01
+	.db "IKKIVECS"
+	.dw vec
+	.dw vec_length
+	.db PRG
+	
+	.db FileDataBlock
+	old_addr = $
+	.base $df80
+	vec:
+		.include "bypass.asm"
+	vec_length = $ - vec
+	.base old_addr + vec_length
+
+; ----------------------------------------------------------------------------------------------------------------------------------
+; CHR
+	.db FileHeaderBlock
+	.db $02, $02
+	.db "IKKICHAR"
+	.dw $0000
+	.dw chr_length
+	.db CHR
+	
+	.db FileDataBlock
+	chr_length = $2000 ; 8KiB
+	.incbin "Ikki (Japan).nes", INES_HDR + prg_length, chr_length
+
+; ----------------------------------------------------------------------------------------------------------------------------------
+; kyodaku file
+	.db FileHeaderBlock
+	.db $03, $03
+	.db "-BYPASS-"
+	.dw $2000
+	.dw $0001
+	.db PRG
+
+	.db FileDataBlock
+	.db $90 ; enable NMI byte loaded into PPU control register - bypasses "KYODAKU-" file check
+	
+; this file will never be loaded but it's big enough for an NMI to kick in while seeking the disk
+	.db FileHeaderBlock
+	.db $04, $FF
+	.db "-BYPASS-"
+	.dw $0000
+	.dw $0400
+	.db PRG
+
+	.db FileDataBlock
+	.dsb $0400
+	
+	.pad 65500
+
